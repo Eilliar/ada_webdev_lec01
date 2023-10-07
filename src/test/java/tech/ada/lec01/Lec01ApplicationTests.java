601117f9ded5337c01e613d6badb50f401cceec7
Bruno Eilliar <engbrunoeilliar@gmail.com>
2023-10-06 22:09:39 -0300

    Initial commit

---
+++ src/test/java/tech/ada/lec01/Lec01ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ada.lec01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lec01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
